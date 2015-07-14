61e64a59ee174b710bdfc7a09e7a154ec269576b
David Morrison <shalompisteuo@gmail.com>
2015-07-13 22:40:28 -0400

    Clean up custom_slidebox.js

--- js/custom_slidebox.js
+++ js/custom_slidebox.js
@@ -1,17 +1,11 @@
 ;(function ($) {
-  Drupal.behaviors.slidebox = {
-    attach: function(context, settings) {
+  $(window).scroll(function() {
+    var distanceTop = $('#slidebox_trigger').offset().top - $(window).height() - 1000;
 
-      $(window).scroll(function() {
-        var distanceTop = $('#slidebox_trigger').offset().top - $(window).height() - 1000;
-
-        if  ($(window).scrollTop() >= distanceTop) {
-          $('#custom-slidebox').animate({'right':'0px'}, 300);
-        }
-        else {
-          $('#custom-slidebox').stop(true).animate({'right': '-' + ($('#custom-slidebox').width() + 30) + 'px'}, 100);
-        }
-      });
+    if  ($(window).scrollTop() >= distanceTop) {
+      $('#custom-slidebox').animate({'right':'0px'}, 300);
+    } else {
+      $('#custom-slidebox').stop(true).animate({'right': '-' + ($('#custom-slidebox').width() + 30) + 'px'}, 100);
     }
-  };
+  });
 })(jQuery);
